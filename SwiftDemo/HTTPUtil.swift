97fe61833c15c58df5e6fd313faa125d2252e7bd
apple <691698513@qq.com>
2021-05-16 22:01:14 +0800

    add file

---
+++ SwiftDemo/HTTPUtil.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkUtil.swift
+//  SwiftDemo
+//
+//  Created by apple on 2021/5/16.
+//
+
+import UIKit
+
+class HTTPUtil: NSObject {
+    
+    func get(url: String,
+             params: [String:String],
+             success: @escaping (Any) -> Void,
+             failure: @escaping (Error) -> Void) -> Void {
+        
+        var items = [URLQueryItem]()
+        for (key,value) in params {
+            items.append(URLQueryItem(name: key, value: value))
+        }
+        let urlComp = NSURLComponents(string: url)!
+        if !items.isEmpty {
+            urlComp.queryItems = items
+        }
+        
+        var urlRequest = URLRequest(url: urlComp.url!)
+        urlRequest.httpMethod = "GET"
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            if (error) {
+                
+            } else {
+                
+            }
+        })
+        task.resume()
+    }
+    
+}
---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by apple on 2021/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
